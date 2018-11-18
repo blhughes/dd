62130aca1933e03bafc0a31b3dcab448f087242c
Bryan Hughes <blhughe@sandia.gov>
2018-11-18 16:18:02 -0700

    Update for DesertDragon 2019
    * New youth age ranges 5-8, 9-13, 13-17
    * New registration rates, 50,60,70
    * New group events
    * New invoice payment method

--- app.py
+++ app.py
@@ -36,7 +36,11 @@ EVENTS={"100": "External Forms Empty Hand Chuan Fa/Kempo",
         "130": "Reaction Skills Moving Step Push Hands",
         "131": "Reaction Skills Shuai Jiao",
         "132": "External Forms Weapons Two Person Ext. Weapon",
-        "133": "Internal Forms Weapons Two Person Int. Weapon"
+        "133": "Internal Forms Weapons Two Person Int. Weapon",
+        "134": "External Forms Group",
+        "135": "External Forms Group Weapons",
+        "136": "Internal Forms Group",
+        "137": "Internal Forms Group Weapons"
 }
 
 fields = ['name','address','phone','email','gender','age','experience',
@@ -49,10 +53,9 @@ EXP_DIVISIONS={ 'novice': 'Novice',
                 'advanced': 'Advanced',
                 'advanced_plus': 'Advanced Plus'
 }
-AGE_DIVISIONS=[ ('Youth 5-7',(5,7)),
-                ('Youth 8-10',(8,10)),
-                ('Youth 11-13',(11,13)),
-                ('Youth 13-17',(14,17)),
+AGE_DIVISIONS=[ ('Youth 5-8',(5,8)),
+                ('Youth 9-10',(9,12)),
+                ('Youth 13-17',(13,17)),
                 ('Adult 18-35',(18,35)),
                 ('Adult 36+',(36,1000))
 ]
@@ -61,18 +64,19 @@ GENDERS=['M','F']
 
 app= Flask(__name__)
 app.config['MONGO_CONNECT']=False
+app.config['MONGO_URI']="mongodb://localhost:27017/app"
 app.config.from_object(__name__)
 
 mongo = PyMongo(app)
 def process_event_data(docs):
   event_data=dict()
   for doc in docs:
-    age_div = filter(lambda x: int(doc['age']) in range(x[1][0],x[1][1]+1),AGE_DIVISIONS)[0]
-    if not doc.has_key('events'):
+    age_div = [x for x in AGE_DIVISIONS if int(doc['age']) in range(x[1][0],x[1][1]+1)][0]
+    if 'events' not in doc:
       doc['events']=list()
     for event in doc['events']:
       key = ( (event,EVENTS[event]),age_div,doc['experience'])
-      if event_data.has_key(key):
+      if key in event_data:
         event_data[key].append(doc)
       else:
         event_data[key] = [doc]
@@ -93,24 +97,24 @@ def registration_form():
 @app.route('/register', methods=['POST'])
 def register():
   competitor = mongo.db.competitors
-  data = { key:request.form[key] for key in request.form.keys() if key in fields}
-  if 'events' in data.keys():
+  data = { key:request.form[key] for key in list(request.form.keys()) if key in fields}
+  if 'events' in list(data.keys()):
     data['events'] = request.form.getlist('events')
-  if 'waiver' in data.keys():
+  if 'waiver' in list(data.keys()):
     if data['waiver'] == 'yes':
       data['waiver'] = True
     else:
       data['waiver'] = False
   else:
       data['waiver'] = False
-  
+
   competitor_id = None
   if request.form['uid'] == '':
     competitor_id = competitor.insert(data)
   else:
     competitor_id = request.form['uid']
     competitor.replace_one({'_id':ObjectId(request.form['uid'])},data)
-  print competitor_id
+  print(competitor_id)
   return redirect(url_for('competitor', uid=competitor_id))
 
 @app.route('/unregister', methods=['POST'])
@@ -118,13 +122,13 @@ def unregister():
   uid = request.form['uid']
   mongo.db.competitors.remove(ObjectId(uid))
   return redirect(url_for('competitors'))
-  
+
 @app.route('/edit/<uid>')
 def edit(uid=None):
   competitor = mongo.db.competitors.find_one(ObjectId(uid))
   competitor['uid']=uid
   return render_template('register.html', doc=competitor)
-  
+
 
 @app.route('/competitors')
 def competitors():
@@ -160,17 +164,16 @@ def stats():
   competitors = [x for x in mongo.db.competitors.find()]
   entries = len(competitors)
   event_data = process_event_data(competitors)
-  recorded_payments = sum( [float(doc['payment_amount']) for doc in competitors if doc.has_key('payment_amount') and doc['payment_amount']!=""]  )
-  events=[doc['events'] for doc in competitors if doc.has_key('events')]
+  recorded_payments = sum( [float(doc['payment_amount']) for doc in competitors if 'payment_amount' in doc and doc['payment_amount']!=""]  )
+  events=[doc['events'] for doc in competitors if 'events' in doc]
   esum=0
   for event in events:
     if(len(event) ==1):
-      esum = esum+40
-    elif(len(event) ==2):
       esum = esum+50
-    elif(len(event) >=3):
+    elif(len(event) ==2):
       esum = esum+60
+    elif(len(event) >=3):
+      esum = esum+70
   payment=esum
- 
-  return render_template('stats.html', docs = competitors, entries=entries, recorded_payments=recorded_payments, payment=payment, events=len(event_data))
 
+  return render_template('stats.html', docs = competitors, entries=entries, recorded_payments=recorded_payments, payment=payment, events=len(event_data))
